5db34a56a51e603fbf57b929cdf8cad2c98b4543
divolgin <dmitriy@replicated.com>
2022-02-04 12:01:38 -0800

    Add more resources to the get command

--- cli/get.go
+++ cli/get.go
@@ -57,13 +57,83 @@ func GetCmd() *cobra.Command {
 			}
 
 			switch args[0] {
+			case "cronjob", "cronjobs":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "cronjobs", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "deployment", "deployments":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "deployments", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "event", "events":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "events", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "ingress", "ingresses":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "ingress", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "job", "jobs":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "jobs", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "limitrange", "limitranges":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "limitranges", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
 			case "pod", "pods":
 				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "pods", v.GetString("namespace"))
 				if err != nil {
 					return errors.Wrap(err, "failed to pring pods")
 				}
+			case "pvc", "pvcs":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "pvcs", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "replicaset", "replicasets", "rs":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "replicasets", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "service", "services", "svc":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "services", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
+			case "statefulset", "statefulsets":
+				err := sbctl.PrintNamespacedGet(clusterData.ClusterResourcesDir, "statefulsets", v.GetString("namespace"))
+				if err != nil {
+					return errors.Wrap(err, "failed to pring pods")
+				}
 			case "ns", "namespace", "namespaces":
 				err := sbctl.PrintClusterGet(clusterData.ClusterResourcesDir, "namespaces")
+				if err != nil {
+					return errors.Wrap(err, "failed to pring namespaces")
+				}
+			case "node", "nodes":
+				err := sbctl.PrintClusterGet(clusterData.ClusterResourcesDir, "nodes")
+				if err != nil {
+					return errors.Wrap(err, "failed to pring namespaces")
+				}
+			case "pv", "pvs":
+				err := sbctl.PrintClusterGet(clusterData.ClusterResourcesDir, "pvs")
+				if err != nil {
+					return errors.Wrap(err, "failed to pring namespaces")
+				}
+			case "resource", "resources":
+				err := sbctl.PrintClusterGet(clusterData.ClusterResourcesDir, "resources")
+				if err != nil {
+					return errors.Wrap(err, "failed to pring namespaces")
+				}
+			case "storageclass", "storageclasses":
+				err := sbctl.PrintClusterGet(clusterData.ClusterResourcesDir, "storage-classes")
 				if err != nil {
 					return errors.Wrap(err, "failed to pring namespaces")
 				}
