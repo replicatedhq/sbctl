aad480c5b2212ea3c34c2a475cfbf8cf722d4b64
divolgin <dmitriy@replicated.com>
2022-02-16 08:00:32 -0800

    API server that works with kubectl

---
+++ cli/api.go
@@ -0,0 +1,88 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/sbctl/pkg/api"
+	"github.com/replicatedhq/sbctl/pkg/sbctl"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func APICmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "api",
+		// Args:          cobra.MinimumNArgs(0),
+		Short:         "Start API server",
+		Long:          `Start API server`,
+		SilenceUsage:  true,
+		SilenceErrors: false,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			viper.BindPFlags(cmd.Flags())
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var kubeConfig string
+			go func() {
+				signalChan := make(chan os.Signal, 1)
+				signal.Notify(signalChan, os.Interrupt)
+				<-signalChan
+				if kubeConfig != "" {
+					_ = os.RemoveAll(kubeConfig)
+				}
+				os.Exit(0)
+			}()
+
+			v := viper.GetViper()
+
+			// This only works with generated config, so let's make sure we don't mess up user's real files.
+			bundlePath := v.GetString("support-bundle-path")
+			if bundlePath == "" {
+				return errors.New("support bundle filename is required or SBCTL_SUPPORT_BUNDLE_PATH must be set")
+			}
+
+			fileInfo, err := os.Stat(bundlePath)
+			if err != nil {
+				return errors.New("failed to stat input path")
+			}
+
+			bundleDir := bundlePath
+			if !fileInfo.IsDir() {
+				bundleDir, err = os.MkdirTemp("", "sbctl-")
+				if err != nil {
+					return errors.Wrap(err, "failed to create temp dir")
+				}
+				defer os.RemoveAll(bundleDir)
+
+				err = sbctl.ExtractBundle(os.Args[1], bundleDir)
+				if err != nil {
+					return errors.Wrap(err, "failed to extract bundle")
+				}
+			}
+
+			clusterData, err := sbctl.FindClusterData(bundleDir)
+			if err != nil {
+				return errors.Wrap(err, "failed to find cluster data")
+			}
+
+			kubeConfig, err = api.StartAPIServer(clusterData)
+			if err != nil {
+				return errors.Wrap(err, "failed to create api server")
+
+			}
+			defer os.RemoveAll(kubeConfig)
+
+			fmt.Printf("Server is running\n\n")
+			fmt.Printf("export KUBECONFIG=%s\n\n", kubeConfig)
+
+			<-make(chan struct{}, 0)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().String("support-bundle-path", "", "path to support bundle archive or directory")
+	return cmd
+}
--- pkg/api/client.go
+++ pkg/api/client.go
@@ -13,41 +13,14 @@ import (
 )
 
 func getClientset(endPoint string) (*kubernetes.Clientset, error) {
-	ctxTemplate := `
-apiVersion: v1
-kind: Config
-preferences: {}
-current-context: default
-clusters:
-- name: default
-  cluster:
-    server: %s
-contexts:
-- name: default
-  context:
-    cluster: default
-    user: default
-users:
-- name: default
-  user: {}
-`
-
-	configString := fmt.Sprintf(ctxTemplate, endPoint)
-	kubeconfigFile, err := ioutil.TempFile("", "local-kubeconfig-")
+	filename, err := createConfigFile(endPoint)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create config file")
 	}
-	defer kubeconfigFile.Close()
 
-	if _, err := io.WriteString(kubeconfigFile, configString); err != nil {
-		return nil, errors.Wrap(err, "failed to write config file")
-	}
-
-	os.Setenv("KUBECONFIG", kubeconfigFile.Name())
-
-	cfg, err := config.GetConfig()
+	cfg, err := getConfigFromfile(filename)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get config")
+		return nil, errors.Wrap(err, "failed to get config from file")
 	}
 
 	clientset, err := kubernetes.NewForConfig(cfg)
@@ -58,7 +31,7 @@ users:
 	return clientset, nil
 }
 
-func getConfig(endPoint string) (*rest.Config, error) {
+func createConfigFile(endPoint string) (string, error) {
 	ctxTemplate := `
 apiVersion: v1
 kind: Config
@@ -81,15 +54,19 @@ users:
 	configString := fmt.Sprintf(ctxTemplate, endPoint)
 	kubeconfigFile, err := ioutil.TempFile("", "local-kubeconfig-")
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to create config file")
+		return "", errors.Wrap(err, "failed to create config file")
 	}
 	defer kubeconfigFile.Close()
 
 	if _, err := io.WriteString(kubeconfigFile, configString); err != nil {
-		return nil, errors.Wrap(err, "failed to write config file")
+		return "", errors.Wrap(err, "failed to write config file")
 	}
 
-	os.Setenv("KUBECONFIG", kubeconfigFile.Name())
+	return kubeconfigFile.Name(), nil
+}
+
+func getConfigFromfile(filename string) (*rest.Config, error) {
+	os.Setenv("KUBECONFIG", filename)
 
 	cfg, err := config.GetConfig()
 	if err != nil {
