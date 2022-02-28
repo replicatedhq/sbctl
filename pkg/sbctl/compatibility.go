8770d756dabd19d4f5cb1f062b20caefa14336ef
Pavan Sokke Nagaraj <pavan@replicated.com>
2022-02-28 14:23:20 -0500

    add sc support

--- pkg/sbctl/compatibility.go
+++ pkg/sbctl/compatibility.go
@@ -15,6 +15,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/kubectl/pkg/scheme"
+	storagev1 "k8s.io/api/storage/v1"
+
 )
 
 func Decode(resource string, data []byte) (runtime.Object, *schema.GroupVersionKind, error) {
