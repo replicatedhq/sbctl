ee0be15bcf60b22872068ddb4a80e3cd26c1ad2f
Pavan Sokke Nagaraj <pavan@replicated.com>
2022-03-03 15:22:59 -0500

    add basic support for `kubectl logs` (#29)
    * add sc support
    
    * fix linters
    
    * Update .gitignore
    
    * add basic support for `k get logs`
    
    * add support to get previous logs
    
    * add correct response headers
    
    * address PR comments

---
+++ pkg/api/logs.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+func (h handler) getAPIV1NamespaceResourceLog(w http.ResponseWriter, r *http.Request) {
+	log.Println("called getAPIV1NamespaceResourceLog")
+
+	namespace := mux.Vars(r)["namespace"]
+	resource := mux.Vars(r)["resource"]
+	name := mux.Vars(r)["name"]
+	container := r.URL.Query().Get("container")
+	previous, _ := strconv.ParseBool(r.URL.Query().Get("previous"))
+
+	logFileName := fmt.Sprintf("%s.log", container)
+	if previous {
+		logFileName = fmt.Sprintf("%s-previous.log", container)
+	}
+
+	fileName := filepath.Join(h.clusterData.ClusterResourcesDir, resource, "logs", namespace, name, logFileName)
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Println("failed to load file", err)
+		if os.IsNotExist(err) {
+			// try reading from -logs-errors.log file
+			errFileName := filepath.Join(h.clusterData.ClusterResourcesDir, resource, "logs", namespace, name, fmt.Sprintf("%s-logs-errors.log", container))
+			data, err = ioutil.ReadFile(errFileName)
+			if err != nil {
+				if os.IsNotExist(err) {
+					PlainText(w, http.StatusNotFound, []byte(fmt.Sprintf("log files not found in support-bundle.\n%v\n%v", fileName, errFileName)))
+					return
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+	PlainText(w, http.StatusOK, data)
+}
+
+func PlainText(w http.ResponseWriter, responseCode int, responseBody []byte) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write(responseBody)
+	w.WriteHeader(responseCode)
+}
