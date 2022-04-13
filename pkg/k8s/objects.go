3a08a587cf40a31c71ffa037b5a5b290e4538fc1
divolgin <dmitriy@replicated.com>
2022-04-13 08:06:50 -0700

    Initial support for label selectors

---
+++ pkg/k8s/objects.go
@@ -0,0 +1,61 @@
+package k8s
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func GetEmptyEventList() *corev1.EventList {
+	r := &corev1.EventList{
+		Items: []corev1.Event{},
+	}
+	r.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+		Version: "v1",
+		Kind:    "EventList",
+	})
+	return r
+}
+
+func GetEmptyPodList() *corev1.PodList {
+	r := &corev1.PodList{
+		Items: []corev1.Pod{},
+	}
+	r.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+		Version: "v1",
+		Kind:    "PodList",
+	})
+	return r
+}
+
+func GetEmptyLimitRangeList() *corev1.LimitRangeList {
+	r := &corev1.LimitRangeList{
+		Items: []corev1.LimitRange{},
+	}
+	r.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+		Version: "v1",
+		Kind:    "LimitRangeList",
+	})
+	return r
+}
+
+func GetEmptyServiceList() *corev1.ServiceList {
+	r := &corev1.ServiceList{
+		Items: []corev1.Service{},
+	}
+	r.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+		Version: "v1",
+		Kind:    "ServiceList",
+	})
+	return r
+}
+
+func GetEmptyPersistentVolumeClaimList() *corev1.PersistentVolumeClaimList {
+	r := &corev1.PersistentVolumeClaimList{
+		Items: []corev1.PersistentVolumeClaim{},
+	}
+	r.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{
+		Version: "v1",
+		Kind:    "PersistentVolumeClaimList",
+	})
+	return r
+}
