ca1b7cca17d7b8b2c3f4df4f3cc5c6ce33193bdd
Evans Mungai <evans@replicated.com>
2023-09-29 11:43:02 +0100

    Remove unused code and deprecated functions

--- pkg/api/client.go
+++ pkg/api/client.go
@@ -3,33 +3,10 @@ package api
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/pkg/errors"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
-
-func getClientset(endPoint string) (*kubernetes.Clientset, error) {
-	filename, err := createConfigFile(endPoint)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create config file")
-	}
-
-	cfg, err := getConfigFromfile(filename)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get config from file")
-	}
-
-	clientset, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create kubernetes clientset")
-	}
-
-	return clientset, nil
-}
 
 func createConfigFile(endPoint string) (string, error) {
 	ctxTemplate := `
@@ -52,7 +29,7 @@ users:
 `
 
 	configString := fmt.Sprintf(ctxTemplate, endPoint)
-	kubeconfigFile, err := ioutil.TempFile("", "local-kubeconfig-")
+	kubeconfigFile, err := os.CreateTemp("", "local-kubeconfig-")
 	if err != nil {
 		return "", errors.Wrap(err, "failed to create config file")
 	}
@@ -63,15 +40,4 @@ users:
 	}
 
 	return kubeconfigFile.Name(), nil
-}
-
-func getConfigFromfile(filename string) (*rest.Config, error) {
-	os.Setenv("KUBECONFIG", filename)
-
-	cfg, err := config.GetConfig()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get config")
-	}
-
-	return cfg, nil
 }
